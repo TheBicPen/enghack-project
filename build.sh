330cd87ed0ab8e8e64429ece6503ed496b312bb0
Alex <mashkal2000@gmail.com>
2019-06-22 17:23:34 -0400

    simplified build.sh a little

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 #assume debian w/ aptitude
 sudo apt install python
 sudo apt install python3
-# sudo apt install python-pip
-sudo apt install python3-pip
+sudo apt install python-pip
+# sudo apt install python3-pip
 
-pip install -r requirements.txt
-pip3 install -r requirements.txt
-pip3 install opencv-python
-pip3 install python-opencv
-pip3 install google-cloud
-pip3 install google-cloud-vision
-pip3 install google-cloud-language
-pip3 install ffmpeg
+# pip install -r requirements.txt
+# pip3 install -r requirements.txt
+pip install opencv-python
+pip install python-opencv
+pip install google-cloud
+pip install google-cloud-vision
+# pip install google-cloud-language
+# pip3 install ffmpeg
 
 sudo apt install python-opencv
 sudo apt install python3-opencv
