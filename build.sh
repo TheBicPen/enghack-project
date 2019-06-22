9b45c5fd00c9b2b4a8cff98228bd17aebe96b621
Alex <mashkal2000@gmail.com>
2019-06-22 10:17:11 -0400

    add reqs file

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
+
+#assume debian w/ aptitude
+sudo apt install python
+sudo apt install python3
+# sudo apt install python-pip
+sudo apt install python3-pip
+
+pip install -r requirements.txt
+pip3 install -r requirements.txt
 pip install opencv-python
+pip install python-opencv
+pip install google-cloud
 pip install google-cloud-vision
 pip install google-cloud-language
 pip install ffmpeg
+
+sudo apt install python-opencv
+sudo apt install python3-opencv
 
  export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/creds.json"
  echo "$(printenv | grep 'GOOGLE_APPLICATION_CREDENTIALS')"
