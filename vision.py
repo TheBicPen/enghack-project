f98a792380874f092c6866f499376d07ad373588
Alex <mashkal2000@gmail.com>
2019-06-22 01:27:18 -0400

    create vision.py

---
+++ vision.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def classify():
+    # Imports the Google Cloud client library
+    from google.cloud import vision
+
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # The text to analyze
+    
+
+if __name__ == '__main__':
+    run_quickstart()
+
